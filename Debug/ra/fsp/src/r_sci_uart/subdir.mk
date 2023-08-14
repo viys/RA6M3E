477ac10e338aebc5d883af0309a3d3b4197a017f
viys <2628587386@qq.com>
2023-08-15 00:54:07 +0800

    first commit

---
+++ Debug/ra/fsp/src/r_sci_uart/subdir.mk
@@ -0,0 +1,26 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../ra/fsp/src/r_sci_uart/r_sci_uart.c 
+
+C_DEPS += \
+./ra/fsp/src/r_sci_uart/r_sci_uart.d 
+
+OBJS += \
+./ra/fsp/src/r_sci_uart/r_sci_uart.o 
+
+SREC += \
+RA6M3E.srec 
+
+MAP += \
+RA6M3E.map 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+ra/fsp/src/r_sci_uart/%.o: ../ra/fsp/src/r_sci_uart/%.c
+	$(file > $@.in,-mcpu=cortex-m4 -mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16 -O2 -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections -Wunused -Wuninitialized -Wall -Wextra -Wmissing-declarations -Wconversion -Wpointer-arith -Wshadow -Wlogical-op -Waggregate-return -Wfloat-equal  -g -gdwarf-4 -D_RENESAS_RA_ -D_RA_CORE=CM4 -I"C:/Users/yugut/Desktop/Workspace/e2studio/RA6M3E/src" -I"C:/Users/yugut/Desktop/Workspace/e2studio/RA6M3E/ra/fsp/inc" -I"C:/Users/yugut/Desktop/Workspace/e2studio/RA6M3E/ra/fsp/inc/api" -I"C:/Users/yugut/Desktop/Workspace/e2studio/RA6M3E/ra/fsp/inc/instances" -I"C:/Users/yugut/Desktop/Workspace/e2studio/RA6M3E/ra/arm/CMSIS_5/CMSIS/Core/Include" -I"C:/Users/yugut/Desktop/Workspace/e2studio/RA6M3E/ra_gen" -I"C:/Users/yugut/Desktop/Workspace/e2studio/RA6M3E/ra_cfg/fsp_cfg/bsp" -I"C:/Users/yugut/Desktop/Workspace/e2studio/RA6M3E/ra_cfg/fsp_cfg" -std=c99 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -c -o "$@" -x c "$<")
+	@echo Building file: $< && arm-none-eabi-gcc @"$@.in"
+
---
+++ Debug/src/hal_entry.d
@@ -0,0 +1,101 @@
+src/hal_entry.o: ../src/hal_entry.c \
+ C:/Users/yugut/Desktop/Workspace/e2studio/RA6M3E/ra_gen/hal_data.h \
+ C:/Users/yugut/Desktop/Workspace/e2studio/RA6M3E/ra/fsp/inc/api/bsp_api.h \
+ C:/Users/yugut/Desktop/Workspace/e2studio/RA6M3E/ra/fsp/inc/api/fsp_common_api.h \
+ C:/Users/yugut/Desktop/Workspace/e2studio/RA6M3E/ra/fsp/inc/fsp_version.h \
+ C:/Users/yugut/Desktop/Workspace/e2studio/RA6M3E/ra_cfg/fsp_cfg/bsp/bsp_cfg.h \
+ C:/Users/yugut/Desktop/Workspace/e2studio/RA6M3E/ra_gen/bsp_clock_cfg.h \
+ C:/Users/yugut/Desktop/Workspace/e2studio/RA6M3E/ra_cfg/fsp_cfg/bsp/bsp_mcu_family_cfg.h \
+ C:/Users/yugut/Desktop/Workspace/e2studio/RA6M3E/ra_cfg/fsp_cfg/bsp/bsp_mcu_device_pn_cfg.h \
+ C:/Users/yugut/Desktop/Workspace/e2studio/RA6M3E/ra_cfg/fsp_cfg/bsp/bsp_mcu_device_cfg.h \
+ c:\users\yugut\desktop\workspace\e2studio\ra6m3e\ra\fsp\src\bsp\mcu\ra6m3\bsp_mcu_info.h \
+ c:\users\yugut\desktop\workspace\e2studio\ra6m3e\ra\fsp\src\bsp\mcu\ra6m3\bsp_elc.h \
+ c:\users\yugut\desktop\workspace\e2studio\ra6m3e\ra\fsp\src\bsp\mcu\ra6m3\bsp_feature.h \
+ C:/Users/yugut/Desktop/Workspace/e2studio/RA6M3E/ra_cfg/fsp_cfg/bsp/board_cfg.h \
+ c:\users\yugut\desktop\workspace\e2studio\ra6m3e\ra\fsp\src\bsp\mcu\all\bsp_arm_exceptions.h \
+ C:/Users/yugut/Desktop/Workspace/e2studio/RA6M3E/ra_gen/vector_data.h \
+ c:\users\yugut\desktop\workspace\e2studio\ra6m3e\ra\fsp\src\bsp\cmsis\device\renesas\include\renesas.h \
+ C:/Users/yugut/Desktop/Workspace/e2studio/RA6M3E/ra/arm/CMSIS_5/CMSIS/Core/Include/cmsis_compiler.h \
+ C:/Users/yugut/Desktop/Workspace/e2studio/RA6M3E/ra/arm/CMSIS_5/CMSIS/Core/Include/cmsis_gcc.h \
+ c:\users\yugut\desktop\workspace\e2studio\ra6m3e\ra\fsp\src\bsp\cmsis\device\renesas\include\R7FA6M3AH.h \
+ C:/Users/yugut/Desktop/Workspace/e2studio/RA6M3E/ra/arm/CMSIS_5/CMSIS/Core/Include/core_cm4.h \
+ C:/Users/yugut/Desktop/Workspace/e2studio/RA6M3E/ra/arm/CMSIS_5/CMSIS/Core/Include/cmsis_version.h \
+ C:/Users/yugut/Desktop/Workspace/e2studio/RA6M3E/ra/arm/CMSIS_5/CMSIS/Core/Include/cmsis_compiler.h \
+ C:/Users/yugut/Desktop/Workspace/e2studio/RA6M3E/ra/arm/CMSIS_5/CMSIS/Core/Include/mpu_armv7.h \
+ c:\users\yugut\desktop\workspace\e2studio\ra6m3e\ra\fsp\src\bsp\cmsis\device\renesas\include\system.h \
+ c:\users\yugut\desktop\workspace\e2studio\ra6m3e\ra\fsp\src\bsp\cmsis\device\renesas\include\system.h \
+ c:\users\yugut\desktop\workspace\e2studio\ra6m3e\ra\fsp\src\bsp\mcu\all\bsp_common.h \
+ c:\users\yugut\desktop\workspace\e2studio\ra6m3e\ra\fsp\inc\api\fsp_common_api.h \
+ c:\users\yugut\desktop\workspace\e2studio\ra6m3e\ra\fsp\src\bsp\mcu\all\bsp_compiler_support.h \
+ c:\users\yugut\desktop\workspace\e2studio\ra6m3e\ra\fsp\src\bsp\mcu\all\bsp_register_protection.h \
+ c:\users\yugut\desktop\workspace\e2studio\ra6m3e\ra\fsp\src\bsp\mcu\all\bsp_irq.h \
+ c:\users\yugut\desktop\workspace\e2studio\ra6m3e\ra\fsp\src\bsp\mcu\all\bsp_io.h \
+ c:\users\yugut\desktop\workspace\e2studio\ra6m3e\ra\fsp\src\bsp\mcu\all\bsp_group_irq.h \
+ c:\users\yugut\desktop\workspace\e2studio\ra6m3e\ra\fsp\src\bsp\mcu\all\bsp_clocks.h \
+ c:\users\yugut\desktop\workspace\e2studio\ra6m3e\ra\fsp\src\bsp\mcu\all\bsp_module_stop.h \
+ c:\users\yugut\desktop\workspace\e2studio\ra6m3e\ra\fsp\src\bsp\mcu\all\bsp_security.h \
+ c:\users\yugut\desktop\workspace\e2studio\ra6m3e\ra\fsp\inc\fsp_features.h \
+ C:/Users/yugut/Desktop/Workspace/e2studio/RA6M3E/ra/fsp/inc/api/fsp_common_api.h \
+ c:\users\yugut\desktop\workspace\e2studio\ra6m3e\ra\fsp\src\bsp\mcu\all\bsp_compiler_support.h \
+ c:\users\yugut\desktop\workspace\e2studio\ra6m3e\ra\fsp\src\bsp\mcu\all\bsp_delay.h \
+ c:\users\yugut\desktop\workspace\e2studio\ra6m3e\ra\fsp\src\bsp\mcu\all\bsp_mcu_api.h \
+ C:/Users/yugut/Desktop/Workspace/e2studio/RA6M3E/ra_gen/common_data.h \
+ C:/Users/yugut/Desktop/Workspace/e2studio/RA6M3E/ra/fsp/inc/instances/r_ioport.h \
+ C:/Users/yugut/Desktop/Workspace/e2studio/RA6M3E/ra/fsp/inc/api/r_ioport_api.h \
+ C:/Users/yugut/Desktop/Workspace/e2studio/RA6M3E/ra/fsp/inc/api/bsp_api.h \
+ C:/Users/yugut/Desktop/Workspace/e2studio/RA6M3E/ra_cfg/fsp_cfg/r_ioport_cfg.h \
+ C:/Users/yugut/Desktop/Workspace/e2studio/RA6M3E/ra_cfg/fsp_cfg/bsp/bsp_pin_cfg.h \
+ C:/Users/yugut/Desktop/Workspace/e2studio/RA6M3E/ra/fsp/inc/instances/r_sci_uart.h \
+ C:/Users/yugut/Desktop/Workspace/e2studio/RA6M3E/ra/fsp/inc/api/r_uart_api.h \
+ C:/Users/yugut/Desktop/Workspace/e2studio/RA6M3E/ra/fsp/inc/api/r_transfer_api.h \
+ C:/Users/yugut/Desktop/Workspace/e2studio/RA6M3E/ra_cfg/fsp_cfg/r_sci_uart_cfg.h
+C:/Users/yugut/Desktop/Workspace/e2studio/RA6M3E/ra_gen/hal_data.h:
+C:/Users/yugut/Desktop/Workspace/e2studio/RA6M3E/ra/fsp/inc/api/bsp_api.h:
+C:/Users/yugut/Desktop/Workspace/e2studio/RA6M3E/ra/fsp/inc/api/fsp_common_api.h:
+C:/Users/yugut/Desktop/Workspace/e2studio/RA6M3E/ra/fsp/inc/fsp_version.h:
+C:/Users/yugut/Desktop/Workspace/e2studio/RA6M3E/ra_cfg/fsp_cfg/bsp/bsp_cfg.h:
+C:/Users/yugut/Desktop/Workspace/e2studio/RA6M3E/ra_gen/bsp_clock_cfg.h:
+C:/Users/yugut/Desktop/Workspace/e2studio/RA6M3E/ra_cfg/fsp_cfg/bsp/bsp_mcu_family_cfg.h:
+C:/Users/yugut/Desktop/Workspace/e2studio/RA6M3E/ra_cfg/fsp_cfg/bsp/bsp_mcu_device_pn_cfg.h:
+C:/Users/yugut/Desktop/Workspace/e2studio/RA6M3E/ra_cfg/fsp_cfg/bsp/bsp_mcu_device_cfg.h:
+c:\users\yugut\desktop\workspace\e2studio\ra6m3e\ra\fsp\src\bsp\mcu\ra6m3\bsp_mcu_info.h:
+c:\users\yugut\desktop\workspace\e2studio\ra6m3e\ra\fsp\src\bsp\mcu\ra6m3\bsp_elc.h:
+c:\users\yugut\desktop\workspace\e2studio\ra6m3e\ra\fsp\src\bsp\mcu\ra6m3\bsp_feature.h:
+C:/Users/yugut/Desktop/Workspace/e2studio/RA6M3E/ra_cfg/fsp_cfg/bsp/board_cfg.h:
+c:\users\yugut\desktop\workspace\e2studio\ra6m3e\ra\fsp\src\bsp\mcu\all\bsp_arm_exceptions.h:
+C:/Users/yugut/Desktop/Workspace/e2studio/RA6M3E/ra_gen/vector_data.h:
+c:\users\yugut\desktop\workspace\e2studio\ra6m3e\ra\fsp\src\bsp\cmsis\device\renesas\include\renesas.h:
+C:/Users/yugut/Desktop/Workspace/e2studio/RA6M3E/ra/arm/CMSIS_5/CMSIS/Core/Include/cmsis_compiler.h:
+C:/Users/yugut/Desktop/Workspace/e2studio/RA6M3E/ra/arm/CMSIS_5/CMSIS/Core/Include/cmsis_gcc.h:
+c:\users\yugut\desktop\workspace\e2studio\ra6m3e\ra\fsp\src\bsp\cmsis\device\renesas\include\R7FA6M3AH.h:
+C:/Users/yugut/Desktop/Workspace/e2studio/RA6M3E/ra/arm/CMSIS_5/CMSIS/Core/Include/core_cm4.h:
+C:/Users/yugut/Desktop/Workspace/e2studio/RA6M3E/ra/arm/CMSIS_5/CMSIS/Core/Include/cmsis_version.h:
+C:/Users/yugut/Desktop/Workspace/e2studio/RA6M3E/ra/arm/CMSIS_5/CMSIS/Core/Include/cmsis_compiler.h:
+C:/Users/yugut/Desktop/Workspace/e2studio/RA6M3E/ra/arm/CMSIS_5/CMSIS/Core/Include/mpu_armv7.h:
+c:\users\yugut\desktop\workspace\e2studio\ra6m3e\ra\fsp\src\bsp\cmsis\device\renesas\include\system.h:
+c:\users\yugut\desktop\workspace\e2studio\ra6m3e\ra\fsp\src\bsp\cmsis\device\renesas\include\system.h:
+c:\users\yugut\desktop\workspace\e2studio\ra6m3e\ra\fsp\src\bsp\mcu\all\bsp_common.h:
+c:\users\yugut\desktop\workspace\e2studio\ra6m3e\ra\fsp\inc\api\fsp_common_api.h:
+c:\users\yugut\desktop\workspace\e2studio\ra6m3e\ra\fsp\src\bsp\mcu\all\bsp_compiler_support.h:
+c:\users\yugut\desktop\workspace\e2studio\ra6m3e\ra\fsp\src\bsp\mcu\all\bsp_register_protection.h:
+c:\users\yugut\desktop\workspace\e2studio\ra6m3e\ra\fsp\src\bsp\mcu\all\bsp_irq.h:
+c:\users\yugut\desktop\workspace\e2studio\ra6m3e\ra\fsp\src\bsp\mcu\all\bsp_io.h:
+c:\users\yugut\desktop\workspace\e2studio\ra6m3e\ra\fsp\src\bsp\mcu\all\bsp_group_irq.h:
+c:\users\yugut\desktop\workspace\e2studio\ra6m3e\ra\fsp\src\bsp\mcu\all\bsp_clocks.h:
+c:\users\yugut\desktop\workspace\e2studio\ra6m3e\ra\fsp\src\bsp\mcu\all\bsp_module_stop.h:
+c:\users\yugut\desktop\workspace\e2studio\ra6m3e\ra\fsp\src\bsp\mcu\all\bsp_security.h:
+c:\users\yugut\desktop\workspace\e2studio\ra6m3e\ra\fsp\inc\fsp_features.h:
+C:/Users/yugut/Desktop/Workspace/e2studio/RA6M3E/ra/fsp/inc/api/fsp_common_api.h:
+c:\users\yugut\desktop\workspace\e2studio\ra6m3e\ra\fsp\src\bsp\mcu\all\bsp_compiler_support.h:
+c:\users\yugut\desktop\workspace\e2studio\ra6m3e\ra\fsp\src\bsp\mcu\all\bsp_delay.h:
+c:\users\yugut\desktop\workspace\e2studio\ra6m3e\ra\fsp\src\bsp\mcu\all\bsp_mcu_api.h:
+C:/Users/yugut/Desktop/Workspace/e2studio/RA6M3E/ra_gen/common_data.h:
+C:/Users/yugut/Desktop/Workspace/e2studio/RA6M3E/ra/fsp/inc/instances/r_ioport.h:
+C:/Users/yugut/Desktop/Workspace/e2studio/RA6M3E/ra/fsp/inc/api/r_ioport_api.h:
+C:/Users/yugut/Desktop/Workspace/e2studio/RA6M3E/ra/fsp/inc/api/bsp_api.h:
+C:/Users/yugut/Desktop/Workspace/e2studio/RA6M3E/ra_cfg/fsp_cfg/r_ioport_cfg.h:
+C:/Users/yugut/Desktop/Workspace/e2studio/RA6M3E/ra_cfg/fsp_cfg/bsp/bsp_pin_cfg.h:
+C:/Users/yugut/Desktop/Workspace/e2studio/RA6M3E/ra/fsp/inc/instances/r_sci_uart.h:
+C:/Users/yugut/Desktop/Workspace/e2studio/RA6M3E/ra/fsp/inc/api/r_uart_api.h:
+C:/Users/yugut/Desktop/Workspace/e2studio/RA6M3E/ra/fsp/inc/api/r_transfer_api.h:
+C:/Users/yugut/Desktop/Workspace/e2studio/RA6M3E/ra_cfg/fsp_cfg/r_sci_uart_cfg.h:
